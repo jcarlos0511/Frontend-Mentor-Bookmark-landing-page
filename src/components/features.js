d4d9fb6f7e9c319ef7d6a0457a12d6cc430c75e0
Chara- <escor05_11@hotmail.com>
2020-07-22 01:40:24 -0500

    features 1/3

--- src/components/features.js
+++ src/components/features.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
+import BookmarkContext from "../context/bookmarkContext";
+import styled from "styled-components";
+import { theme } from "../styles";
+
+const { fontSizes } = theme;
+const StyledContainer = styled.div`
+  border: 1px solid green;
+  margin: 0 8%;
+  padding: 2.2em 0;
+  text-align: center;
+`;
+
+const StyledTitle = styled.h2`
+  font-size: ${fontSizes.xl};
+  font-weight: 500;
+`;
+
+const StyledDescription = styled.p`
+  font-size: ${fontSizes.md};
+  line-height: 1.65em;
+`;
 
 const Features = () => {
-  return <div>Holi, soy features!</div>;
+  const { data } = useContext(BookmarkContext);
+  const { sections } = data;
+
+  const { features } = sections;
+  const { title, description } = features;
+
+  return (
+    <StyledContainer>
+      <StyledTitle>{title}</StyledTitle>
+      <StyledDescription>{description}</StyledDescription>;
+    </StyledContainer>
+  );
 };
 
 export default Features;
--- src/data/index.js
+++ src/data/index.js
@@ -22,7 +22,11 @@ export const data = {
         description:
           "A clean and simple interface to organize your favourite websites. Open a new browser tab and see your sites load instantly. Try it for free.",
       },
-      features: {},
+      features: {
+        title: "Features",
+        description:
+          "Our aim is to make it quick and easy for you to access your favourite websites. Your bookmarks sync between your devices so you can access them on the go.",
+      },
       pricing: {},
       faq: {},
       contact: {},
